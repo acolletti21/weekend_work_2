03e9955b4f413d815a98938ecfb309ea7557ab37
acolletti21 <ashleymcolletti@gmail.com>
2017-01-22 14:22:05 -0600

    completed work

--- oop/yodel.rb
+++ oop/yodel.rb
@@ -10,6 +10,17 @@
 
 class Yodel
 
+  attr_reader :segment, :count
+
+  def initialize(input_options)
+    @segment = input_options[:segment]
+    @count = input_options[:count]
+  end
+
+  def articulate
+    segment * count
+  end
+
 end
 
 
